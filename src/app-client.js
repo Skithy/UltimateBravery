69285eabd2aacd022b18fa2cc24d87e3fb7be23c
Skithy <skithment@gmail.com>
2017-01-01 23:48:23 +1100

    plz work

--- src/app-client.js
+++ src/app-client.js
@@ -3,29 +3,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import AppRoutes from './components/AppRoutes'
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-
-import {cyan500, purple500, green500} from 'material-ui/styles/colors';
-
-const muiTheme = getMuiTheme({
-  palette: {
-    primary1Color: cyan500,
-    primary2Color: purple500,
-    primary3Color: green500,
-  },
-}, {
-  avatar: {
-    borderColor: null,
-  },
-  userAgent: req.headers['user-agent'],
-});
 
 const App = () => (
-  <MuiThemeProvider muiTheme={muiTheme}>
     <AppRoutes />
-  </MuiThemeProvider>
 )
 
 window.onload = () => {
--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -3,11 +3,15 @@
 import React from 'react';
 import { Router, browserHistory } from 'react-router';
 import routes from '../routes';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 export default class AppRoutes extends React.Component {
   render() {
     return (
-      <Router history={browserHistory} routes={routes} onUpdate={() => window.scrollTo(0, 0)}/>
+      <MuiThemeProvider muiTheme={getMuiTheme({userAgent: navigator.userAgent})}>
+        <Router history={browserHistory} routes={routes} onUpdate={() => window.scrollTo(0, 0)}/>
+      </MuiThemeProvider>
     );
   }
 }
