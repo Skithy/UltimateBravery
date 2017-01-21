3b70fc138fb7987471134e0325ac21d6c41d12af
Skithy <skithment@gmail.com>
2017-01-22 05:11:42 +1100

    dice initial animation

--- src/components/DicePage/DiceSection.js
+++ src/components/DicePage/DiceSection.js
@@ -4,35 +4,52 @@ import React from 'react'
 import NumericInput from 'react-numeric-input'
 import Button from 'react-bootstrap/lib/Button'
 
-const DiceSection = ({dice, updateValues, generateValues}) => (
-  <div>
-    <h1>Dice Roll</h1>
-    <div id="decription" style={{marginBottom:15}}>
-      <p style={{display:"inline-block", marginRight:20}}>Number of dice: </p>
-      <NumericInput
-          min={1} max={9}
-          value={dice.length}
-          onChange={updateValues}
-          maxLength={1}
-          className="form-control"
-      />
-    </div>
+export default class DiceSection extends React.Component {
+  constructor() {
+    super()
+    this.state = {rotation: 0}
+    this.rotate = this.rotate.bind(this)
+  }
 
-    <div className="display display-dice">
-      {dice.map((value, index) =>
-        <span key={index}
-          className={"mdi mdi-dice-" + value}
-          style={{fontSize:"calc(7vh + 7vw)", margin:"auto"}}
-        />
-      )}
-    </div>
+  rotate() {
+    this.setState(
+      {rotation: this.state.rotation + 360},
+      this.props.generateValues
+    )
+  }
 
-    <div id="buttons">
-      <Button bsStyle="primary" onClick={generateValues}>Roll Dice</Button>
-    </div>
+  render() {
+    return(
+      <div>
+        <h1>Dice Roll</h1>
+        <div id="decription" style={{marginBottom:15}}>
+          <p style={{display:"inline-block", marginRight:20}}>Number of dice: </p>
+          <NumericInput
+              min={1} max={9}
+              value={this.props.dice.length}
+              onChange={this.props.updateValues}
+              maxLength={1}
+              className="form-control"
+          />
+        </div>
 
-  </div>
-)
+        <div className="display display-dice">
+          {this.props.dice.map((value, index) =>
+            <span key={index}
+              className={"mdi mdi-dice-" + value}
+              style={{fontSize:"calc(7vh + 7vw)", margin:"auto", transform:"rotate(" + this.state.rotation + "deg)"}}
+            />
+          )}
+        </div>
+
+        <div id="buttons">
+          <Button bsStyle="primary" onClick={this.rotate}>Roll Dice</Button>
+        </div>
+
+      </div>
+    )
+  }
+}
 
 const getDiceSize = num => {
   switch(num) {
@@ -41,5 +58,3 @@ const getDiceSize = num => {
     case 9: return "calc(10px + 10vw)"
   }
 }
-
-export default DiceSection
